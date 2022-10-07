d4ec134ddfa6fb48ac7a835da5ec23edbc1dbf23
tomatogrove <66643214+tomatogrove@users.noreply.github.com>
2022-10-06 20:25:07 -0500

    added skeleton of state management and XML parser

---
+++ src/main/car/functionality/StateController.java
@@ -0,0 +1,15 @@
+package car.functionality;
+
+import car.inventory.Dealership;
+
+import java.util.List;
+
+public class StateController {
+
+    public static List<Dealership> loadState() {
+        return null;
+    }
+
+    public static void saveState(List<Dealership> dealers) { }
+
+}
---
+++ src/main/car/functionality/VehicleXMLParser.java
@@ -0,0 +1,18 @@
+package car.functionality;
+
+import car.inventory.Dealership;
+import car.inventory.Vehicle;
+
+import java.io.File;
+import java.util.List;
+
+public class VehicleXMLParser {
+
+    public static List<Vehicle> read(String filePath) {
+        return null;
+    }
+
+    public static void write(Dealership dealer) { }
+
+    public static void writeAll(List<Dealership> dealers, File file) { }
+}
